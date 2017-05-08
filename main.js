0c54235272e991d93a60bb52d98d5cdd554a4f23
CarolinaTapia <cstapiarubio@gmail.com>
2017-05-08 13:38:38 -0300

    cambios añadidos

--- main.js
+++ main.js
@@ -3,4 +3,10 @@
 
 	var valorPeso = document.getElementById("peso").value;
 	var valorEstatura = document.getElementById("alto").value;
-	alert((valorPeso / Math.pow(valorEstatura,2)).toFixed(2));
+	var resultado = "";
+
+    function imc (valorPeso, valorEstatura){ 
+    	return resultado=((valorPeso / Math.pow(valorEstatura,2)).toFixed(2));
+    }
+    alert("Tu IMC es " + resultado );
+
