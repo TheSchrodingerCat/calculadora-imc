8ddc34fc48fe88f6199139db903ddce3968167dc
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-08 12:58:09 -0300

    html y js sin funciones

---
+++ main.js
@@ -0,0 +1,6 @@
+
+
+
+	var valorPeso = document.getElementById("peso").value;
+	var valorEstatura = document.getElementById("alto").value;
+	alert((valorPeso / Math.pow(valorEstatura,2)).toFixed(2));
